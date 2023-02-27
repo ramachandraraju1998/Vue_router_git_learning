a124d26e249f61c1dc1d65cec4285d3c3cf4f577
ramachandraraju1998 <50516380+ramachandraraju1998@users.noreply.github.com>
2023-02-27 08:56:29 +0530

    changes done in home component

--- components/home.js
+++ components/home.js
@@ -1,4 +1,4 @@
 const Home = {
 	template:` <p class="h2 fst-italic">Welcome to CarTrade Tech Ltd.</p>
-	<router-link class="btn btn-primary" to="/modal">Launch First Modal</router-link>`
+	<router-link class="btn btn-primary" to="/modal">Launch First Modal Testing</router-link>`
 }
